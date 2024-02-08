58232e47823b3cf1462e2fbe253c281f40e3bcd0
Vivek Bigelow <vivekbigelow1234@gmail.com>
2024-02-07 19:18:56 -0500

    Add nav links and style homepage

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import Face from '../components/Face'
 
@@ -7,15 +8,17 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Vivek Bigelow</title>
+        <link rel="icon" href="/img/face/front.jpg"></link>
       </Head>
 
       <main className={styles.main}>
+        <nav className={styles.nav}>
+          <Link className={styles.navLink} href="/about">About</Link>
+          <Link className={styles.navLink} href="/blog">Blog</Link>
+          <Link className={styles.navLink} href="/projects">Projects</Link>
+        </nav>
         <Face />
       </main>
-
-      <footer className={styles.footer}>
-       <p>Copyright Vivek Bigelow 2024</p>
-      </footer>
     </div>
   )
 }
