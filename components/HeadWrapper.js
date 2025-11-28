8c04aa5c7d8e84c6d0f8e80fbc7fd5a8e885ae96
Vivek Bigelow <vivekbigelow1234@gmail.com>
2025-11-28 14:15:43 -0500

    Rebrand to Spastic Bombastic

--- components/HeadWrapper.js
+++ components/HeadWrapper.js
@@ -2,8 +2,8 @@ import Head from 'next/head';
 export default function HeadWrapper() {
     return (
         <Head>
-            <title>Vivek Bigelow</title>
-            <link rel="icon" href="/img/face/front.jpg"></link>
+            <title>Spastic Bombastic</title>
+            <link rel="icon" href="/img/smileyguy.gif"></link>
         </Head>
     )
 }
